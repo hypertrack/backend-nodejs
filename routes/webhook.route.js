eb3dd8fbf3db5c9ab9d3cde0db3ac9f9c1d64e93
Alexander Graebe <alex.graebe@gmail.com>
2019-08-01 13:01:33 -0700

    receive new v2 hypertrack webhooks

--- routes/webhook.route.js
+++ routes/webhook.route.js
@@ -43,4 +43,47 @@ module.exports = app => {
 
     res.sendStatus(200);
   });
+
+  // Receive all HyperTrack webhooks
+  app.post("/hypertrack-v2", async function(req, res) {
+    let webhookBody = JSON.parse(req.body);
+
+    // log request body
+    console.log(webhookBody);
+
+    if (webhookBody) {
+      for (let i = 0; i < webhookBody.length; i++) {
+        let data = webhookBody[i];
+
+        // notify client
+        res.io.emit(data.type, data);
+
+        switch (data.type) {
+          case "location":
+            console.log("==== LOCATION UPDATE");
+            // webhook.addLocation(data);
+            break;
+          case "summary":
+            console.log("==== SUMMARY UPDATE");
+            // webhook.addSummary(data);
+            break;
+          case "activity":
+            console.log("==== ACTIVITY UPDATE");
+            // webhook.addActivity(data);
+            break;
+          case "health":
+            console.log("==== HEALTH UPDATE");
+            // webhook.addHealth(data);
+            break;
+          case "trip":
+            console.log("==== TRIP UPDATE");
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    res.sendStatus(200);
+  });
 };
