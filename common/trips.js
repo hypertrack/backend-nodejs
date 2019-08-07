5af2e136553f7c0a21520a03233ab2b6e20b5971
Alexander Graebe <alex.graebe@gmail.com>
2019-08-06 18:30:30 -0700

    Fix heroku button setup

--- common/trips.js
+++ common/trips.js
@@ -2,6 +2,9 @@ function createTripsForAllDevices() {
   let output = {};
 
   console.log("Creating new trips ...");
+  // mongo: get all devices
+
+  // mongo create new trips for all devices
 
   return output;
 }
