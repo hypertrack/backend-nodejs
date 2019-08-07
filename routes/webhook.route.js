01516b8beb49451f06ccdf0ccdb0b88c9b54da5f
Alexander Graebe <alex.graebe@gmail.com>
2019-08-07 16:00:16 -0700

    avoid processing v1 webhooks

--- routes/webhook.route.js
+++ routes/webhook.route.js
@@ -5,35 +5,36 @@ module.exports = app => {
   app.post("/hypertrack", async function(req, res) {
     let webhookBody = JSON.parse(req.body);
 
-    // log request body
-    console.log(webhookBody);
-
     if (webhookBody) {
       for (let i = 0; i < webhookBody.length; i++) {
         let data = webhookBody[i];
 
-        // notify client
-        res.io.emit(data.type, data);
+        // TODO: Remove after v1 deprecation
+        if (data.version === "2.0.0") {
+          console.log(data);
+          // notify client
+          res.io.emit(data.type, data);
 
-        switch (data.type) {
-          case "location":
-            console.log("==== LOCATION UPDATE");
-            webhook.addLocation(data);
-            break;
-          case "device_status":
-            console.log("==== DEVICE UPDATE");
-            webhook.addDeviceStatus(data);
-            break;
-          case "battery":
-            console.log("==== BATTERY UPDATE");
-            webhook.addBatteryStatus(data);
-            break;
-          case "trip":
-            console.log("==== TRIP UPDATE");
-            webhook.addTripStatus(data);
-            break;
-          default:
-            break;
+          switch (data.type) {
+            case "location":
+              console.log("==== LOCATION UPDATE");
+              webhook.addLocation(data);
+              break;
+            case "device_status":
+              console.log("==== DEVICE UPDATE");
+              webhook.addDeviceStatus(data);
+              break;
+            case "battery":
+              console.log("==== BATTERY UPDATE");
+              webhook.addBatteryStatus(data);
+              break;
+            case "trip":
+              console.log("==== TRIP UPDATE");
+              webhook.addTripStatus(data);
+              break;
+            default:
+              break;
+          }
         }
       }
     }
