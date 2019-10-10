6cd28e0fbc2ebe1abb3fa682c64bed341e3979f0
Alexander Graebe <alex.graebe@gmail.com>
2019-10-10 15:39:19 -0700

    Adding ability to paginate through Trips API responses

--- common/trips.js
+++ common/trips.js
@@ -1,4 +1,6 @@
 var request = require("request");
+var rp = require("request-promise");
+var tripCollection = require("../models/trip.model");
 var _ = require("lodash");
 require("dotenv").config();
 
@@ -101,47 +103,6 @@ function createTripsForAllDevices() {
   });
 }
 
-function completeDailyTripsForallDevices() {
-  // get all active trips using HyperTrack API
-  const base64auth = Buffer.from(
-    `${process.env.HT_ACCOUNT_ID}:${process.env.HT_SECRET_KEY}`
-  ).toString("base64");
-  const auth = `Basic ${base64auth}`;
-  let options = {
-    url: "https://v3.api.hypertrack.com/trips",
-    headers: {
-      Authorization: auth
-    }
-  };
-
-  request(options, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      const trips = JSON.parse(body);
-
-      trips.forEach(trip => {
-        // complete only daily scheduled trips
-        if (_.get(trip, "metadata.scheduled_trip", false)) {
-          options = {
-            url: `https://v3.api.hypertrack.com/trips/${trip.trip_id}/complete`,
-            method: "POST",
-            headers: {
-              Authorization: auth
-            }
-          };
-
-          request(options, (error, response, body) => {
-            if (!error && response.statusCode == 202) {
-              console.log(
-                `Trip completed for device_id '${trip.device_id}': ${trip.trip_id}`
-              );
-            }
-          });
-        }
-      });
-    }
-  });
-}
-
 function completeTrip(tripId) {
   // complete trip by trip_id using HyperTrack API
   const base64auth = Buffer.from(
@@ -163,7 +124,7 @@ function completeTrip(tripId) {
   });
 }
 
-function updateAllTrips() {
+async function updateAllTrips() {
   // get all trips (completed and active) using HyperTrack API
   const base64auth = Buffer.from(
     `${process.env.HT_ACCOUNT_ID}:${process.env.HT_SECRET_KEY}`
@@ -175,41 +136,49 @@ function updateAllTrips() {
       Authorization: auth
     }
   };
+  let trips = [];
+  let bulkOps = [];
+  let tripResponse;
 
-  request(options, (error, response, body) => {
-    if (!error && response.statusCode == 200) {
-      const trips = JSON.parse(body);
-      let bulkOps = [];
+  while (options.url !== null) {
+    console.log("Pagination through all trips with URL: ", options.url);
+    await rp(options)
+      .then(function(body) {
+        tripResponse = JSON.parse(body);
+        trips.push(tripResponse.data);
 
-      // update all trips in mongoDB
-      var tripCollection = require("../models/trip.model");
+        // set up pagination
+        options.url = _.get(tripResponse, "links.next", null);
 
-      if (trips && Array.isArray(trips)) {
-        trips.forEach(trip => {
-          let upsertDoc = {
-            updateOne: {
-              filter: { trip_id: trip["trip_id"] },
-              update: trip,
-              upsert: true,
-              setDefaultsOnInsert: true
-            }
-          };
-          bulkOps.push(upsertDoc);
-        });
-
-        if (bulkOps.length > 0) {
-          tripCollection.bulkWrite(bulkOps);
+        // update all trips in mongoDB
+        if (tripResponse.data && Array.isArray(tripResponse.data)) {
+          tripResponse.data.forEach(trip => {
+            let upsertDoc = {
+              updateOne: {
+                filter: { trip_id: trip["trip_id"] },
+                update: trip,
+                upsert: true,
+                setDefaultsOnInsert: true
+              }
+            };
+            bulkOps.push(upsertDoc);
+          });
         }
-      }
-    }
-  });
+      })
+      .catch(function(err) {
+        console.log("Error getting all trips: ", err);
+      });
+  }
+
+  if (bulkOps.length > 0) {
+    tripCollection.bulkWrite(bulkOps);
+  }
 }
 
 module.exports = {
   getTrip,
   createTrip,
   createTripsForAllDevices,
-  completeDailyTripsForallDevices,
   updateAllTrips,
   completeTrip
 };
